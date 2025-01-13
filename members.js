46fa80ee8132bfe354c17bc8ed77dbeefe8cdc7c
Chamara Liyanage <43173294+cliy5602@users.noreply.github.com>
2025-01-13 16:51:09 +0000

    Copliot second commit

---
+++ members.js
@@ -0,0 +1,9 @@
+function skillMembers() {
+    var members = [
+        { name: 'John', skills: ['HTML', 'CSS', 'JS'] },
+        { name: 'Jane', skills: ['HTML', 'CSS', 'JS', 'React'] },
+        { name: 'Jack', skills: ['HTML', 'CSS', 'JS', 'React', 'Node'] }
+    ];
+
+    return members;
+}
